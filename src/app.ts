99293b0405e5c9a3964920af6d7fa1c531ca3c96
rybak2Z <rybak2Z.000@gmail.com>
2023-12-31 16:31:02 +0100

    Set up better debugging

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,30 @@
 import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
+import Debug from "debug";
+
+const debug = Debug("app");
 
 const requiredEnvVariables = ["PORT", "MONGODB_URL"];
 for (const variable of requiredEnvVariables) {
   if (!(variable in process.env)) {
-    console.error(
+    debug(
       `Missing environment variable '${variable}'. Make sure to configure the .env file correctly.`,
     );
     process.exit(1);
   }
 }
 
-connectToDb().catch((err) => console.log(err));
+connectToDb().catch((err) => debug(err));
 async function connectToDb() {
-  console.log("Connecting to database...");
+  debug("Attempting to connect to database...");
   await mongoose.connect(process.env.MONGODB_URL as string);
-  console.log("...connected!");
+  debug("Successfully connected to database!");
 }
 
 const app = express();
 const port = parseInt(process.env.PORT as string, 10);
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  debug(`Listening on port ${port}`);
 });
