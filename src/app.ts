650fb5628e2bc5a8b42d5290a2110470c89a2b13
rybak2Z <rybak2Z.000@gmail.com>
2023-12-31 11:16:43 +0100

    Set up database connection

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,27 @@
+import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
+
+const requiredEnvVariables = ["PORT", "MONGODB_URL"];
+for (const variable of requiredEnvVariables) {
+  if (!(variable in process.env)) {
+    console.error(
+      `Missing environment variable '${variable}'. Make sure to configure the .env file correctly.`,
+    );
+    process.exit(1);
+  }
+}
+
+connectToDb().catch((err) => console.log(err));
+async function connectToDb() {
+  console.log("Connecting to database...");
+  await mongoose.connect(process.env.MONGODB_URL as string);
+  console.log("...connected!");
+}
+
 const app = express();
-const port = 3000;
+const port = parseInt(process.env.PORT as string, 10);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
